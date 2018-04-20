55250051d14f9eea57f0c47006366ec9337ec1db
Matt Mitchell <mattmitchell2718@gmail.com>
2018-04-20 16:29:42 -0600

    Revised Landau damping initialization

--- common.c
+++ common.c
@@ -6,27 +6,21 @@
 #include "common.h"
 
 const double XMAX = 16.0; // system length
-const int NGRID = 64; // grid size
+const int NGRID = 256; // grid size
 double DX;
 
 // particle number and properties
-const int PART_NUM = 10000;
+const int PART_NUM = 5000;
 const double PART_MASS = 0.005;
-const double PART_CHARGE = -0.02;
+const double PART_CHARGE = -0.01;
 const double EPS_0 = 1.0;
 
 const double BEAM_SPEED = 8.0;
 
 double OMEGA_P;
 
-// helper function for initRhoSin
-static double bisect(double x1, double x2, double y, double XMAX) {
-	double xmid = (x1 + x2) / 2;
-	double ymid = xmid + 0.25 * XMAX/(2*M_PI) * (1 - cos(2 * M_PI * xmid/XMAX));
-	if (ymid - y > 1e-9) return bisect(x1, xmid, y, XMAX);
-	else if (ymid - y < -1e-9) return bisect(xmid, x2, y, XMAX);
-	else return xmid;
-}
+// wave periods per system length for standing wave and landau damping
+const int WAVE_MODE = 3;
 
 // 2-stream instability, standard test case
 void init2Stream(double *x, double *v, int *color) {
@@ -58,53 +52,56 @@ void initStanding(double *x, double *v, int *color) {
 	OMEGA_P = sqrt((PART_NUM/XMAX) * (PART_CHARGE*PART_CHARGE)
 	               / (PART_MASS * EPS_0));
 	
-	int mode = 9;
-	double ampl = 0.2;
+	double ampl = 0.3;
 	
 	for (int i = 0; i < PART_NUM; i++) {
 		x[i] = i * XMAX / PART_NUM;
-		x[i] += ampl * XMAX  / (2 * M_PI * mode)
-			* cos(2 * M_PI * mode * x[i] / XMAX);
+		x[i] += ampl * XMAX  / (2 * M_PI * WAVE_MODE)
+			* cos(2 * M_PI * WAVE_MODE * x[i] / XMAX);
 
-		v[i] = ampl * OMEGA_P * XMAX / (2 * M_PI * mode)
-			* sin(2 * M_PI * mode * x[i] / XMAX);
+		v[i] = ampl * OMEGA_P * XMAX / (2 * M_PI * WAVE_MODE)
+			* sin(2 * M_PI * WAVE_MODE * x[i] / XMAX);
 
 		color[i] = 0x0000ff;
 	}
 }
 
-// wave in maxwellian, used to test Landau damping
-void initLandau(double *x, double *v, int *color) {
-	int mode = 3;
-	double ampl = 0.5;
-	double stddev = sqrt(8);
+// helper function for initLandau
+// use newton's method to find inverse of position CDF
+static double newton(double u) {
+	double k = WAVE_MODE * 2*M_PI/XMAX;
+	double ampl = 0.4;
 	
+	// find root of x/L + a/(k L) sin(k x) - u = 0
+	// derivative is 1/L + a/L cos(k x)
+	double x = u * XMAX;
+	double f = x/XMAX + ampl / (k * XMAX) * sin(k * x) - u;
+
+	while (fabs(f) > 1e-9) {
+		double fprime = 1/XMAX + ampl / XMAX * cos(k * x);
+		x -= f / fprime;
+		f = x/XMAX + ampl / (k * XMAX) * sin(k * x) - u;
+	}
+
+	return x;
+}
+
+// wave in maxwellian, used to test Landau damping
+// distribution function: f(x,v) = exp(-1/2 (v/v_th)^2) / (sqrt(2 Pi) * v_th)
+//                                 * (1 + a cos(k x)) / L
+void initLandau(double *x, double *v, int *color) {
+	double vth = 1; // sqrt(kt/m)
+
 	for (int i = 0; i < PART_NUM; i++) {
-		// spatially uniform
-		x[i] = i * XMAX / PART_NUM;
+		// charge distribution produced via inverse transform sampling
+		x[i] = newton(i / (double)PART_NUM);
 
 		// init maxwellian distro
 		// box-mueller
 		double r1 = (rand() + 1) / ((double)RAND_MAX + 1); // log(0) breaks stuff
 		double r2 = (rand() + 1) / ((double)RAND_MAX + 1);
-		v[i] = stddev * sqrt(-2 * log(r1)) * cos(2 * M_PI * r2);
-
-		// wave perturbation
-		x[i] += ampl * XMAX  / (2 * M_PI * mode)
-			* sin(2 * M_PI * mode * x[i] / XMAX);
+		v[i] = vth * sqrt(-2 * log(r1)) * cos(2 * M_PI * r2);
 		
-		color[i] = 0x0000ff;
-
-
-	}
-}
-
-// sinusoidal charge dist with no initial velocity
-// not super useful as a test case, but can make sure the code isn't broken
-void initRhoSin(double *x, double *v, int *color) {
-	for (int i = 0; i < PART_NUM; i++) {
-		x[i] = bisect(0, XMAX, i * XMAX / PART_NUM, XMAX);
-		v[i] = 0;
 		color[i] = 0x0000ff;
 	}
 }
