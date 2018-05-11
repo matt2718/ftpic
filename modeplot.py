dab1e5759965e685e074024310b31dd079d833fd
Matt Mitchell <mattmitchell2718@gmail.com>
2018-05-11 16:55:31 -0600

    Modified mode plotting script

--- modeplot.py
+++ modeplot.py
@@ -5,19 +5,46 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-if len(sys.argv) < 3: exit()
+# parse arguments
+decay = None
+files = {}
+plots = []
 
-modename = sys.argv[1]
+i = 1
+while i < len(sys.argv):
+	arg = sys.argv[i]
+	if arg == '-d':
+		# theoretical decay rate
+		i += 1
+		if i == len(sys.argv): quit(2)
+		decay = float(sys.argv[i])
+	else:
+		# arguments take the form mode,file
+		split = arg.find(',')
+		if split > 0:
+			mode = arg[:split]
+			fname = arg[split+1:]
+			plots.append((mode,fname))
+			files[fname] = None
+	i += 1
 
-plt.figure(num=modename)
+# read files
+for fname in files:
+	files[fname] = pd.read_csv(fname)
 
-for myfile in sys.argv[2:]:
-	rawdata = pd.read_csv(myfile)
+plt.figure()
+
+for modename,fname in plots:
+	rawdata = files[fname]
 
 	time = rawdata['time'].values
-	mode = rawdata[modename].values
+	mode = rawdata['m' + modename].values
 
-	plt.plot(time, np.log10(mode), label=myfile)
+	plt.plot(time, np.log10(mode), label = modename + ',' + fname)
+
+if decay != None:
+	y0 = np.log10(mode[0])
+	plt.plot([0, 5], [y0, y0 - 5/np.log(10) * decay])
 
 plt.xlabel('Time')
 plt.ylabel('log(E_' + modename + ')')
